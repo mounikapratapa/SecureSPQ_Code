703fefc98155510df3f0b5e870f3bcec1dfe0da6
mounikapratapa <43151963+mounikapratapa@users.noreply.github.com>
2024-05-09 10:35:44 -0400

    Update qr-keygen.py

--- qr-keygen.py
+++ qr-keygen.py
@@ -1,194 +1,212 @@
-import sys
-from miller_rabin_test import is_prime
+from gmpy2 import kronecker, is_prime, mpz
+import gmpy2
+import numpy as np
+from sympy.ntheory import factorint
+import random
+import galois
+from random import randint
+from functools import reduce
 import time
 start_time = time.time()
-# f() is the function to implement
-# The convention we'll use is that f(i)=0 denotes a quadratic residue
-# (1), and f(i)=1 denotes a non-residue (-1)
-f = [0] * 20 + [1] * 20
-beta = 11
-alpha = 42
-k = 41
+a = 2
+b = 1
+k = 512
+"""a, b are used to generate the sequence and k is the size of the sequence, 
+which in-turn denotes the size of the function. 
+Here 512 indicates it is a 9 bit function"""
 
-sequence = [alpha * i + beta  for i in range(k)]
+f =  [1] * 512
+""" f() is the function to implement. We used an example domain that has 
+exactly 50% zeros and 50% ones. The convention we'll use is that: 
+f(i)=0 denotes a quadratic residue (1), and f(i)= 1 denotes a non-residue (-1)"""
+
+sequence = [a * i + b for i in range(k)]
+
+def gcd(a, b):
+    """returns the greatest common divisor of a and b"""    
+    
+    while b:
+        return gcd(b, a % b)
+    return a
+
+def lcm(a, b):
+    return abs(a * b) // gcd(a, b)
+
+def XGCD(a, b):
+    """Extended GCD:
+    Returns (gcd, x, y), where gcd is greatest common divisor of a and b.
+    The numbers x,y are such that gcd = ax+by."""
+    prevx, x = 1, 0
+    prevy, y = 0, 1
+    while b:
+        q, r = divmod(a, b)
+        x, prevx = prevx - q * x, x
+        y, prevy = prevy - q * y, y
+        a, b = b, r
+    return a, prevx, prevy
+
+def crt(a,b,m=None,n=None):
+    """
+    This function is similar to the crt algorithm for sagemath's
+    miscellaneous arithmetic functions library. We adopted it for python3.
+    
+    It returns a solution to Chinese Remainder problem, where:
+    - a, b are two residues / two lists- one of residues and
+      one of moduli.
+    - m, n are two moduli. In case a, b are lists the values of m, n are none.
+    - if m, n are given, the function returns a solution x to the
+    simultaneous congruences "x = a (mod m)" and "x =  b (mod n)", if one exists.
+    - According to Chinese remainder theorem, a solution to the
+    simultaneous congruences exists if and only if
+    "a =  b (mod(gcd(m,n))". 
+    - The solution x is  well-defined in "(mod lcm(m,n))".
+    - If a, b are lists, then m=n=None and the crt function returns solution to:
+        "x =  a_i (mod b_i)", if it exists.    
+    """
+    if isinstance(a, list):
+        return CRT_list(a, b)
+    if isinstance(a, (int)): 
+        a = int(a) # otherwise q, r = divmod(b-a, g) doesnt work
+    g, alpha, beta = XGCD(m, n)
+    q, r = divmod(b-a, g)
+    if r != 0:
+        raise ValueError("No solution to crt problem since gcd(%s,%s) does not divide %s-%s"
+                         % (m, n, a, b))
+    return (a + (q*alpha*m)) % lcm(m, n)
+
+CRT = crt
+
+def CRT_list(v, moduli):
+    """ Given a list v of elements and a list of corresponding
+    moduli, find a single element that reduces to each element of
+    v modulo the corresponding moduli."""
+    if not isinstance(v,list) or not isinstance(moduli,list):
+        raise ValueError("Arguments to CRT_list should be lists")
+    if len(v) != len(moduli):
+        raise ValueError("Arguments to CRT_list should be lists of the same length")
+    if len(v) == 0:
+        return int(0)
+    if len(v) == 1:
+        return moduli[0].parent()(v[0])
+    x = v[0]
+    m = moduli[0]
+    for i in range(1,len(v)):
+        x = CRT(x,v[i],m,moduli[i])
+        m = lcm(m,moduli[i])
+    return x%m
 
 
-# The power of an factor a in integer b
-# Returns an integer n where b=a^n*c
+
+
+
+
+
+def find_quadratic_residues(number_list):
+    """given a list of prime factors, returns the list of ALL quadratic residues
+    with respect to each prime factor ai to form as a
+    look up table"""
+    results = []
+
+    for number in number_list:
+        residues = []
+        
+        for x in range(1, number):
+            if kronecker(-x, number) == 1:
+                residues.append(x)
+            
+
+        results.append((number, residues))
+
+    return results
+
+
+def find_random_element_with_symbol( residues):
+    """from a given list of residues, returns a random choice"""
+    return random.choice(residues)
+    
+
+def compute_factor_base(seq):
+    """Determines the set of unique prime factors seen across a sequence
+    Ignores prime factors of even power"""
+
+    fbase = []
+    for element in seq:
+        factors = factorint(element)
+        for factor, power in factors.items():
+            if power % 2 == 1 and factor % 2 == 1 and factor not in fbase:
+                fbase.append(factor)
+    return fbase
+
+
 def compute_power(a, b):
     power = 0
     while gcd(a, b) > 1:
-        b = int(b / a)
+        b = int(b // a)
         power += 1
     return power
 
 
-# Convert elements from function codomain to Legendre symbols
-def map_fn_to_symbol(x):
-    if x == 0:
-        return 1
-    elif x == 1:
-        return -1
+
+def compute_prime(bi, factor_base):
+    """Returns a squre-free prime number satisfying the crt between
+    specified in bi and factor_base of required size"""
+    
+    p = crt(bi, factor_base)
+    x = reduce(lambda a, b: a * b, factor_base)
+    p = p + x
+    #while p.bit_length() < size:
+        #kn = randint(1, random.getrandbits(size))
+        #p = p + kn * x
+    return p, x
 
 
-# Return the index of the first non-zero element in a vector a
-def find_first_nonzero(a_row):
-    for i in range(len(a_row)):
-        if a_row[i] != 0:
-            return i
-
-
-# Given a symbol s\in {1,-1} and a prime p, return a random element in
-# Z^*_p with symbol s
-def find_random_element_with_symbol(s, p):
-    while True:
-        x = randint(1, p - 1)
-        if kronecker(x, p) == s:
-            return x
-
-
-# Determine the set of unique prime factors seen across a sequence
-# Ignores prime factors of even power
-def compute_factor_base():
-    factor_base = []
-    for sequence_value in sequence:
-        for prime_factor in factor(sequence_value):
-            if prime_factor[1] % 2 == 1:
-                if prime_factor[0] not in factor_base:
-                    factor_base.append(prime_factor[0])
-    return factor_base
-
-
-# Returns a matrix A of the sequence expressed as a linear system in
-# GF(2). Rows represent each element of the sequence. Columns represent
-# the presence or absence of each prime factor in that sequence number.
-# The last column represents the intented function output at that
-# sequence number s, i.e. f(s)
-def compute_linear_system(factor_base):
-    # Build the linear system.
-    system_of_congruences = []
-
-    # For each element i of seq, create a binary vector corresponding to
-    # whether j is an odd factor of i
-    for i in range(len(sequence)):
-        sequence_value = sequence[i]
-
-        # For prime factor of the factor base, check whether it is present
-        # in the given sequence number
-        for prime_factor in factor_base:
-            if compute_power(prime_factor, sequence_value) % 2 == 1:
-                system_of_congruences.append(1)
-            else:
-                system_of_congruences.append(0)
-        # Append function value
-        system_of_congruences.append(f[i])
-
-    M = MatrixSpace(GF(2), k, len(factor_base) + 1)
-    return M(system_of_congruences)
-
-
-# Return a squre-free prime number satisfying the system of congruence
-# specified in li and factor_base
-def compute_prime(li, factor_base):
-    p = crt(li, factor_base)
-    x = prod(factor_base)
-
-    while not is_prime(p) or (p % 4 != 1):
-        p = p + x
-
-    return p
-
-
-# ------------------------------------------------------------
 def run():
-    factor_base = compute_factor_base()
+    factor_base = compute_factor_base(sequence)
 
     if 2 in factor_base:
-        print "ERROR: Factor base contains 2, which may lead to inconsistant results. Please select a sequence containing only odd integers"
+        print("ERROR: Factor base contains 2, which may lead to inconsistent results. Please select a sequence containing only odd integers")
         return
 
-    A = compute_linear_system(factor_base)
+   
+    print("")
+    print("ai =", factor_base)
+    res_list = find_quadratic_residues(factor_base)
+    r = []
+    for num, residues in res_list:
+        r.append(residues)
+        
+    bitime = time.time()
+    bi = []
 
-    # Row-reduce echelon A
-    A_reduced = A.echelon_form()
-    
-    
-
-    print ""
-    print "Here's the factored elements of the sequence:"
-    #for i in sequence:
-        #print factor(i)
-    print ""
-    print "Here are the unique odd-powered factors of the elements of the sequence"
-    print factor_base
-    print ""
-    print "Here's a GF(2) matrix showing the system of equations."
-    print "Each row is a sequence number. Each column is a prime factor, except for the last column, which is what the function should evaluate to."
-    #print A
-    print ""
-    print "Here's the matrix in row-reduced echelon form"
-    #print A_reduced
-    print("system to matrix space--- %s seconds ---" % (time.time() - start_time))
-    
-
-    # Check is system is inconsistant
-    for row in A_reduced.rows():
-
-        # Check if all elements of the row (except for the last column) are
-        # zero. If so, there's no solution for this variable and the system
-        # is inconsistant
-        if row[0:-1].is_zero():
-            inconsistant = True
-            print "ERROR: System is inconsistent. Pick a different a,b"
-            return
-    
-    print("Checkinf for consistency--- %s seconds ---" % (time.time() - start_time))
-    
-
-    prime_factor_symbols = [1] * len(factor_base)
-
-    # For each row, find the index of the first non-zero element.
-    for row in A_reduced.rows():
-        # Set the symbol of that factor to the function value
-        # Symbol of -1 if row[-1]=0, and Symbol of 1 if row[-1]=1
-        prime_factor_symbols[find_first_nonzero(row)] = map_fn_to_symbol(
-            int(row[-1]))
-
-    print ""
-    #for i in range(len(factor_base)):
-        #print str(factor_base[i]) + " needs Legendre symbol " + str(
-         #int(prime_factor_symbols[i]))
-    print ""
-    print "ai = " + str(factor_base)
-
-    # Find random elements mod each of the factors of the factor base with
-    # the required symbols
-    li = []
     for i in range(len(factor_base)):
-        li.append(
-            find_random_element_with_symbol(prime_factor_symbols[i],
-                                            factor_base[i]))
-    #print "li = " + str(li)
-    
-    print("Finding b_i--- %s seconds ---" % (time.time() - start_time))
-    
+        bi.append(find_random_element_with_symbol(r[i]))
 
-    p = compute_prime(li, factor_base)
+    print("bi =", bi)
+    print("Finding b_i--- %s seconds ---" % (time.time() - bitime))
 
-    print "p = " + str(p)
-    
-    print("CRT--- %s seconds ---" % (time.time() - start_time))
+    #crttime = time.time()
 
-    print ""
-    print(sequence)
-    print "Testing symbols of sequence modulo p:"
+    p, x = compute_prime(bi, factor_base)
+
+    findingptime = time.time()
+    while  not (is_prime(mpz(p)) and p % 4 == 3):
+        #print("Could not find prime, trying again")
+        bi = []
+
+        for i in range(len(factor_base)):
+            bi.append(find_random_element_with_symbol(r[i]))
+
+        p, x = compute_prime(bi, factor_base)
+
+    print("The final prime is =", p)
+    print("Total Time --- %s seconds ---" % (time.time() - findingptime))
     for i in range(len(sequence)):
-        print kronecker(sequence[i], p)
-        if map_fn_to_symbol(f[i]) != kronecker(sequence[i], p):
-            print "ERROR: symbols of sequence not implement function f"
+       #print(kronecker(sequence[i], p))
+        if f[i] != kronecker(sequence[i], p):
+            print("ERROR: symbols of sequence not implement function f")
             return
-    print "SUCCESS: Symbols of sequence modulo p implement function f"
+    print("SUCCESS: Symbols of sequence modulo p implement function f")
 
 
-# ---------
+
 run()
