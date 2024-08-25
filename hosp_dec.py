cfe4dd159d6571fe2be28e2312ba21e8b11627a7
mounikapratapa <43151963+mounikapratapa@users.noreply.github.com>
2024-08-25 15:52:23 -0400

    Update hosp_dec.py

--- hosp_dec.py
+++ hosp_dec.py
@@ -3,12 +3,6 @@
 from Okamoto_Uchiyama import dec
 import time
 start = time.time()
-p = 70404470836673456642937968870480997199552877944781723511551237182454544842985007390607138146394452777
-q=816923218412868614402781105203
-'''
-The p,q used here are just for example purposes. Generate the actual parameters using qr-keygen.py
-'''
-sk =  p,q
 
 
 
@@ -16,8 +10,10 @@ with open('g.txt', 'r') as file:
         g = file.readlines()
         g = int(''.join(g))
 
-'''with open('sk.txt', 'r') as file:
-        sk = file.readlines()'''
+with open('sk.txt', 'r') as file:
+        sk = file.readlines()
+sk =  p,q
+
 EvalScores=[]
 with open('decrypt.txt', 'r') as file:
         for c in file:
